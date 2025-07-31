aa3f4d884eb4ae07e8acdc22d9dda7047f8ea328
juniwalk <juniwalk@outlook.cz>
2025-07-31 17:04:25 +0200

    Added clear button next to filter input

--- src/Filters/AbstractFilter.php
+++ src/Filters/AbstractFilter.php
@@ -11,12 +11,15 @@ use JuniWalk\DataTable\Column;
 use JuniWalk\DataTable\Columns\Interfaces\Filterable;
 use JuniWalk\DataTable\Exceptions\InvalidStateException;
 use JuniWalk\DataTable\Filter;
+use JuniWalk\DataTable\Traits\Linking;
 use JuniWalk\Utils\Format;
 use Nette\Application\UI\Control;
 use Nette\Application\UI\Form;
 
 abstract class AbstractFilter extends Control implements Filter
 {
+	use Linking;
+
 	protected bool $isFiltered = false;
 	protected mixed $value;
 
@@ -99,11 +102,14 @@ abstract class AbstractFilter extends Control implements Filter
 		}
 
 		$className = Format::className($this, suffix: 'Filter');
+		$clearLink = $this->createLink('clear!', ['column' => $this->name]);
 
 		/** @var \Nette\Bridges\ApplicationLatte\DefaultTemplate */
 		$template = $this->getTemplate();
 		$template->setFile(__DIR__.'/../templates/filter-'.$className.'.latte');
 
+		$template->add('isFiltered', $this->isFiltered);
+		$template->add('clearLink', $clearLink);
 		$template->add('label', $this->label);
 		$template->add('name', $this->name);
 		$template->add('input', $input);
