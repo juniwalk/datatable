59f24204451872a7c844f39646bdde1f7687b562
juniwalk <juniwalk@outlook.cz>
2025-09-30 11:04:18 +0200

    Use onAnchor to check that there is Table in hierarchy

--- src/Filters/AbstractFilter.php
+++ src/Filters/AbstractFilter.php
@@ -139,11 +139,14 @@ abstract class AbstractFilter extends Component implements Filter
 
 	protected function validateParent(IContainer $parent): void
 	{
-		parent::validateParent($parent);
-
-		$this->monitor($this::class, fn() => $this->lookup(Table::class));
 		$this->monitor(Table::class, function(Table $table) {
 			$this->setTranslator($table->getTranslator());
 		});
+
+		parent::validateParent($parent);
+
+		$this->onAnchor[] = function() {
+			$this->lookup(Table::class);
+		};
 	}
 }
