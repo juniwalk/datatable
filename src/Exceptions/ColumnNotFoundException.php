1ba5410248aa372dd7f54258c75dd8fa19571985
juniwalk <juniwalk@outlook.cz>
2025-11-15 00:05:10 +0100

    Allow throwing ColumnNotFound using class name

--- src/Exceptions/ColumnNotFoundException.php
+++ src/Exceptions/ColumnNotFoundException.php
@@ -7,8 +7,19 @@
 
 namespace JuniWalk\DataTable\Exceptions;
 
+use JuniWalk\DataTable\Column;
+
 final class ColumnNotFoundException extends AbstractTableException
 {
+	/**
+	 * @param class-string<Column> $class
+	 */
+	public static function fromClass(string $class): static
+	{
+		return new static('Column "'.$class.'" not found in the table.');
+	}
+
+
 	public static function fromName(string $name): static
 	{
 		return new static('Column "'.$name.'" not found in the table.');
